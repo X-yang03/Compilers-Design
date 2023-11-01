294e0f5ec2688d79e119823c9cb9a9b8cbcbb523
X-yang03 <1979954151@qq.com>
2023-11-02 00:36:54 +0800

    Type

--- lab4/include/Type.h
+++ lab4/include/Type.h
@@ -8,15 +8,24 @@ class Type
 private:
     int kind;
 protected:
-    enum {INT, VOID, FUNC,FLOAT};
+    enum {INT, FLOAT, CONST_INT, CONST_FLOAT, VOID, BOOL, FUNC, INT_ARRAY, FLOAT_ARRAY, CONST_INT_ARRAY, CONST_FLOAT_ARRAY};
 public:
     Type(int kind) : kind(kind) {};
     virtual ~Type() {};
     virtual std::string toStr() = 0;
     bool isInt() const {return kind == INT;};
-    bool isFloat() const {return kind == FLOAT;}
-    bool isVoid() const {return kind == VOID;};
-    bool isFunc() const {return kind == FUNC;};
+    bool isFloat() const {return kind == FLOAT;};
+    bool isConstInt() const {return kind == CONST_INT;}
+    bool isConstFloat() const {return kind == CONST_FLOAT;}
+    bool isBool() const {return kind == BOOL;}
+    bool isVoid() const {return kind == VOID;}
+    bool isFunc() const {return kind == FUNC;}
+    bool isIntArray() const {return kind == INT_ARRAY;}
+    bool isFloatArray() const {return kind == FLOAT_ARRAY;}
+    bool isConstIntArray() const {return kind == CONST_INT_ARRAY;}
+    bool isConstFloatArray() const {return kind == CONST_FLOAT_ARRAY;}
+    bool isArray() const {return kind == INT_ARRAY || kind == FLOAT_ARRAY || 
+                            kind == CONST_FLOAT_ARRAY || kind == CONST_INT_ARRAY;}
 };
 
 class IntType : public Type
@@ -28,6 +37,34 @@ public:
     std::string toStr();
 };
 
+class FloatType : public Type
+{
+public:
+    FloatType() : Type(Type::FLOAT){};
+    std::string toStr();
+};
+
+class ConstIntType : public Type
+{
+public:
+    ConstIntType() : Type(Type::CONST_INT){};
+    std::string toStr();
+};
+
+class ConstFloatType : public Type
+{
+public:
+    ConstFloatType() : Type(Type::CONST_FLOAT){};
+    std::string toStr();
+};
+
+class BoolType : public Type
+{
+public:
+    BoolType() : Type(Type::BOOL){};
+    std::string toStr();
+};
+
 class VoidType : public Type
 {
 public:
@@ -35,14 +72,6 @@ public:
     std::string toStr();
 };
 
-class FloatType : public Type
-{
-private:
-    int size;
-public:
-    FloatType(int size):Type(Type::FLOAT),size(size){};
-    std::string toStr();
-};
 
 class FunctionType : public Type
 {
@@ -56,15 +85,65 @@ public:
     std::string toStr();
 };
 
+class IntArrayType : public Type
+{
+private:
+    std::vector<int> dimensions;
+public:
+    IntArrayType() : Type(Type::INT_ARRAY){};
+    void pushBackDimension(int);
+    std::vector<int> getDimensions();
+    std::string toStr();
+};
+
+class FloatArrayType : public Type
+{
+private:
+    std::vector<int> dimensions;
+public:
+    FloatArrayType() : Type(Type::FLOAT_ARRAY){};
+    void pushBackDimension(int);
+    std::vector<int> getDimensions();
+    std::string toStr();
+};
+
+class ConstIntArrayType : public Type
+{
+private:
+    std::vector<int> dimensions;
+public:
+    ConstIntArrayType() : Type(Type::CONST_INT_ARRAY){};
+    void pushBackDimension(int);
+    std::vector<int> getDimensions();
+    std::string toStr();
+};
+
+class ConstFloatArrayType : public Type
+{
+private:
+    std::vector<int> dimensions;
+public:
+    ConstFloatArrayType() : Type(Type::CONST_FLOAT_ARRAY){};
+    void pushBackDimension(int);
+    std::vector<int> getDimensions();
+    std::string toStr();
+};
+
 class TypeSystem
 {
 private:
     static IntType commonInt;
-    static VoidType commonVoid;
     static FloatType commonFloat;
+    static ConstIntType commonConstInt;
+    static ConstFloatType commonConstFloat;
+    static BoolType commonBool;
+    static VoidType commonVoid;
 public:
     static Type *intType;
     static Type *floatType;
+    static Type *constIntType;
+    static Type *constFloatType;
+    static Type *boolType;
     static Type *voidType;
 };
 
--- lab4/src/Type.cpp
+++ lab4/src/Type.cpp
@@ -2,26 +2,47 @@
 #include <sstream>
 
 IntType TypeSystem::commonInt = IntType(4);
+FloatType TypeSystem::commonFloat = FloatType();
+ConstIntType TypeSystem::commonConstInt = ConstIntType();
+ConstFloatType TypeSystem::commonConstFloat = ConstFloatType();
+BoolType TypeSystem::commonBool = BoolType();
 VoidType TypeSystem::commonVoid = VoidType();
-FloatType TypeSystem::commonFloat = FloatType(4);
 
 Type* TypeSystem::intType = &commonInt;
-Type* TypeSystem::voidType = &commonVoid;
 Type* TypeSystem::floatType = &commonFloat;
+Type* TypeSystem::constIntType = &commonConstInt;
+Type* TypeSystem::constFloatType = &commonConstFloat;
+Type* TypeSystem::boolType = &commonBool;
+Type* TypeSystem::voidType = &commonVoid;
 
 std::string IntType::toStr()
 {
     return "int";
 }
 
-std::string VoidType::toStr()
-{
-    return "void";
-}
-
 std::string FloatType::toStr()
 {
     return "float";
+}
+
+std::string ConstIntType::toStr()
+{
+    return "const int";
+}
+
+std::string ConstFloatType::toStr()
+{
+    return "const float";
+}
+
+std::string BoolType::toStr()
+{
+    return "bool";
+}
+
+std::string VoidType::toStr()
+{
+    return "void";
 }
 
 void FunctionType::setparamsType(std::vector<Type*> in)
@@ -39,4 +60,63 @@ std::string FunctionType::toStr()
     }
     buffer << ")";
     return buffer.str();
+}
+
+void IntArrayType::pushBackDimension(int dim)
+{
+    dimensions.push_back(dim);
+}
+
+std::vector<int> IntArrayType::getDimensions()
+{
+    return dimensions;
+}
+
+std::string IntArrayType::toStr()
+{
+    return "int array";
+}
+
+void FloatArrayType::pushBackDimension(int dim)
+{
+    dimensions.push_back(dim);
+}
+
+std::vector<int> FloatArrayType::getDimensions()
+{
+    return dimensions;
+}
+
+std::string FloatArrayType::toStr()
+{
+    return "float array";
+}
+void ConstIntArrayType::pushBackDimension(int dim)
+{
+    dimensions.push_back(dim);
+}
+
+std::vector<int> ConstIntArrayType::getDimensions()
+{
+    return dimensions;
+}
+
+std::string ConstIntArrayType::toStr()
+{
+    return "const int array";
+}
+
+void ConstFloatArrayType::pushBackDimension(int dim)
+{
+    dimensions.push_back(dim);
+}
+
+std::vector<int> ConstFloatArrayType::getDimensions()
+{
+    return dimensions;
+}
+
+std::string ConstFloatArrayType::toStr()
+{
+    return "const float array";
 }
